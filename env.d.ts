68954f156ed68192a62fc01ab7d1d9d54e382c21
VOIGHT <moriorpandemic@gmail.com>
2025-11-13 21:34:47 +0500

    feat(env): implement google ads config via environment

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_GOOGLE_CLIENT_ID: string
+    readonly VITE_GOOGLE_CLIENT_SECRET: string
+    readonly VITE_GOOGLE_REDIRECT_URI: string
+    readonly VITE_GOOGLE_TOKEN_URI: string
+    readonly VITE_GOOGLE_AUTH_URI: string
+    readonly VITE_GOOGLE_CERTS_URL: string
+    readonly VITE_GOOGLE_PROJECT_ID: string
+    readonly VITE_GOOGLE_ORIGIN: string
+    readonly VITE_GOOGLE_ADS_DEV_TOKEN: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
---
+++ src/config/google.ts
@@ -0,0 +1,11 @@
+export const googleConfig = {
+    clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    clientSecret: import.meta.env.VITE_GOOGLE_CLIENT_SECRET,
+    redirectUri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
+    tokenUri: import.meta.env.VITE_GOOGLE_TOKEN_URI,
+    authUri: import.meta.env.VITE_GOOGLE_AUTH_URI,
+    certsUrl: import.meta.env.VITE_GOOGLE_CERTS_URL,
+    projectId: import.meta.env.VITE_GOOGLE_PROJECT_ID,
+    origin: import.meta.env.VITE_GOOGLE_ORIGIN,
+    googleAdsDevToken: import.meta.env.VITE_GOOGLE_ADS_DEV_TOKEN,
+}
